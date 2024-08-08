5b7fc4754ca2e3a866206587d76ff2d9ace494bc
suman154 <sumanbhatta735@gmail.com>
2024-08-08 16:27:29 +0545

    Register in admin.py

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from network.models import *
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Follower)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
